0e3e585423ce3db699de8aeef546d1edc4bddb6c
Kevin Bertram <kbertr@saic.edu>
2016-11-02 18:16:34 -0500

    Update header link

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Kevin Bertram</a>
+          <a class="navbar-brand" href="/">Kevin Bertram</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
